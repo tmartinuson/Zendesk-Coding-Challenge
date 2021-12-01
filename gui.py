c919fe4861d58d393a9af212fd2d919606d15933
Tristan Martinuson <tmartinuson@gmail.com>
2021-12-01 00:38:49 -0800

    Added GUI implementation and basic views of tickets
    Implemented the GUI for this ticket viewer which includes:
    - A horizontal scrolling list of 25 bulk tickets to view at a time
    - Clickable list members that pull up the ticket to the right side with a summary of data
    - Login page for Zendesk credentials

---
+++ gui.py
@@ -0,0 +1,180 @@
+from pathlib import Path
+
+from tkinter import Frame, Label, Listbox, Scrollbar, Tk, Canvas, Entry, Text, Button, PhotoImage, Toplevel
+from tkinter.constants import ACTIVE, BOTTOM, BROWSE, CENTER, LEFT, RIGHT, SINGLE, TOP
+from types import CellType
+import requests
+from datetime import datetime
+
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./assets")
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+def grabJson(pageNum, Auth):
+    URL = "https://zcctmart.zendesk.com/api/v2/tickets.json?per_page=25&page=" + str(pageNum)
+    return requests.get(url=URL, auth=Auth).json()
+
+class GUI:
+    def __init__(self, data, Auth) -> None:
+        self.data = data
+        self.listbox = None
+        self.master = None
+        self.pageLabel = None
+        self.page = 1
+        self.Auth = Auth
+        self.run()
+
+    def buildTicket(self, ticket):
+        # Select items to display in the list
+        ans = (str(ticket["id"]), ticket["subject"], "Opened by: " + str(ticket["requester_id"]), "on " + str(datetime.strptime(ticket["created_at"], "%Y-%m-%dT%H:%M:%SZ").date()))
+        return " ".join(ans)
+
+    def buildTicketFrame(self, ticketID, master):
+        # TODO: Must build a try except block as some json tags are not required
+        ticket = self.data["tickets"][(int(ticketID)%25)-1]
+        frame = Frame( master=master , bg="#FFFFFF" )
+        subject = Label( frame, text="Ticket Subject:", pady=4, font=("NotoSerif Regular", 15 * -1), fg="#283423", bg="#FFFFFF")
+        subject.pack( side=TOP )
+        title = Label( frame, text="\"" + ticket["subject"] + "\"", pady=4, font=("NotoSerif Regular", 15 * -1), fg="#283423", bg="#FFFFFF")
+        title.pack( side=TOP )
+        submitter = Label( frame, text="Owner ID: " + str(ticket["requester_id"]), pady=4, font=("NotoSerif Regular", 15 * -1), fg="#283423", bg="#FFFFFF")
+        submitter.pack( side=TOP )
+        frame.place(
+            x=455,
+            y=122,
+            width=420
+        )
+
+    def clickListEvent(self, event):
+        ticket = self.listbox.get(self.listbox.curselection())
+        self.buildTicketFrame( ticketID=ticket.split(" ")[0], master=self.master)
+
+    def clickChangePage(self, dir):
+        if self.page+dir == 0:
+            return
+        data = grabJson(pageNum=self.page+dir, Auth=self.Auth)
+        if data is not None and data["tickets"]:
+            self.setupList(values=data)
+            self.page += dir
+            self.data = data
+            self.pageLabel['text'] = str(self.page)
+
+    def setupList(self, values):
+        self.listbox.delete(0,'end')
+        counter = 0
+        for i in values["tickets"]:
+            counter += 1
+            self.listbox.insert(counter, self.buildTicket(i))
+
+    def run(self):
+        window = Tk()
+        window.title("Zendesk Ticket Viewer")
+        window.geometry("901x640")
+        window.configure(bg = "#FFFFFF")
+
+
+        canvas = Canvas(
+            window,
+            bg = "#FFFFFF",
+            height = 640,
+            width = 901,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+
+        canvas.place(x = 0, y = 0)
+        image_image_1 = PhotoImage(
+            file=relative_to_assets("image_1.png"))
+        canvas.create_image(
+            449.25694465637207,
+            319.8826389312744,
+            image=image_image_1
+        )
+
+        canvas.create_text(
+            352.2659606933594,
+            60.692359924316406,
+            anchor="nw",
+            text="Ticket Viewer",
+            fill="#283423",
+            font=("NotoSerif Regular", 30 * -1)
+        )
+
+        canvas.create_text(
+            262.7916564941406,
+            23.150693893432617,
+            anchor="nw",
+            text="Zendesk Coding Challenge",
+            fill="#283423",
+            font=("NotoSerif Regular", 30 * -1)
+        )
+
+        if self.data is None or not self.data["tickets"]:
+            canvas.create_text(
+                262.7916564941406,
+                350,
+                anchor="nw",
+                text="Oops...Unable to load tickets",
+                fill="#283423",
+                font=("NotoSerif Regular", 30 * -1)
+            )
+            window.mainloop()
+            return
+
+        buttonFrame = Frame( 
+            master=window,
+            bg="#FFFFFF"
+        )
+
+        tickets = Listbox(
+            master = buttonFrame,
+            selectmode = SINGLE,
+            height = 25,
+            borderwidth=0,
+            width = 100
+        )
+        self.listbox = tickets
+        self.master = window
+        self.setupList( self.data )
+        self.buildTicketFrame( self.data["tickets"][0]["id"], window )
+        tickets.pack( side=TOP )
+        leftButton = Button(
+            buttonFrame,
+            text="Prev",
+            fg="#283423",
+            bg="#FFFFFF",
+            command=lambda: self.clickChangePage(dir=-1)
+        )
+        leftButton.pack( side=LEFT )
+        rightButton = Button(
+            buttonFrame,
+            text="Next",
+            fg="#283423",
+            bg="#FFFFFF",
+            command=lambda: self.clickChangePage(dir=1)
+        )
+        rightButton.pack( side=RIGHT )
+        buttonFrame.place(
+            x=20,
+            y=120,
+            width=430
+        )
+        pageNum = Entry(
+            buttonFrame,
+            text=str(self.page),
+            fg="#283423",
+            bg="#FFFFFF",
+            width=2
+        )
+        self.pageLabel=pageNum
+        pageNum.pack( side=BOTTOM )
+        tickets.bind('<<ListboxSelect>>', self.clickListEvent)
+        
+        window.resizable(False, False)
+        window.mainloop()
+
+
--- main.py
+++ main.py
@@ -1,12 +1,81 @@
 import requests
-import flask
+import gui
+from tkinter import Frame, Label, Tk, Entry, Button, messagebox
+from tkinter.constants import LEFT, RIGHT, TOP
 
-URL = "https://zcctmart.zendesk.com/api/v2/tickets"
-Auth = ('#####', '#####')
-# Softcode username and password by entry
-re = requests.get(url=URL, auth=Auth)
+# URL = "https://zcctmart.zendesk.com/api/v2/tickets"
+URL = "https://zcctmart.zendesk.com/api/v2/tickets.json?per_page=25&page=1"
 
-data = re.json()
+# Login page
+window = Tk()
+window.title("Login Page")
+window.geometry("450x200")
+window.configure(bg = "#FFFFFF")
 
-for i in data["tickets"]:
-    print(i["subject"])
+def tryLoginAndJson( user , passw ):
+    try:
+        auth = (user, passw)
+        re = requests.get(url=URL, auth=auth)
+        if not re.ok:
+            raise Exception("Login failed")
+        window.destroy()
+        g = gui.GUI(re.json(), auth)
+    except Exception:
+        messagebox.showerror(title="Login failed", message="Username and/or password are incorrect. Please try again")
+
+f = Frame(
+    master=window,
+    bg="#FFFFFF"
+)
+t = Label(
+    f,
+    text="Login details:",
+    bg="#FFFFFF"
+)
+t.pack( side=TOP )
+frameUser = Frame(
+    f,
+    bg="#FFFFFF"
+)
+user = Label(
+    frameUser,
+    text="Email: ",
+    bg="#FFFFFF",
+    padx=14
+)
+user.pack( side=LEFT )
+userEntry = Entry(
+    frameUser,
+    bg="#FFFFFF"
+)
+userEntry.pack( side=RIGHT )
+frameUser.pack( side=TOP )
+framePass = Frame(
+    f,
+    bg="#FFFFFF"
+)
+passw = Label(
+    framePass,
+    text="Password: ",
+    bg="#FFFFFF"
+)
+passw.pack( side=LEFT )
+passEntry = Entry(
+    framePass,
+    bg="#FFFFFF",
+    show="*"
+)
+passEntry.pack( side=RIGHT )
+framePass.pack( side=TOP )
+enter = Button(
+    f,
+    text="Enter",
+    bg="#FFFFFF",
+    command=lambda: tryLoginAndJson(userEntry.get(), passEntry.get())
+)
+enter.pack( side=TOP )
+f.place(
+    x = 100,
+    y = 50
+)
+window.mainloop()
